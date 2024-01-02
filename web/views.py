df127afc2acd979ce30e2a0562d79dc947266bb5
ashwabu <abdulazizredi90@gmail.com>
2024-01-02 08:59:59 +0300

    wizard view

--- web/views.py
+++ web/views.py
@@ -1,3 +1,13 @@
+from formtools.wizard.views import SessionWizardView
+from .forms import RegistrationStep1Form, RegistrationStep2Form
 from django.shortcuts import render
 
-# Create your views here.
+class RegistrationWizardView(SessionWizardView):
+    form_list = [RegistrationStep1Form, RegistrationStep2Form]
+    template_name = 'registration_wizard.html'
+
+    def done(self, form_list, **kwargs):
+        username = form_list[0].cleaned_data['username']
+        email = form_list[0].cleaned_data['email']
+        password = form_list[1].cleaned_data['password1']
+        return render(self.request, 'registration_done.html', {})
