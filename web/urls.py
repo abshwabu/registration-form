1d2d8661288fb0ac9fa964247a2b1ef7bb15114b
ashwabu <abdulazizredi90@gmail.com>
2024-01-05 08:19:38 +0300

    home

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistrationWizardView
+from .views import RegistrationWizardView,home
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('',home, name='home'),
     path('register/', RegistrationWizardView.as_view(), name='registration_wizard'),
     path('register/success/', TemplateView.as_view(template_name='success.html'), name='registration_success'),
     # Add other URLs as needed
